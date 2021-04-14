801a4da7a985b5d131b382afa4d99cf77a74e10e
Dazikiri Anikar <mierzwinski.b@gmail.com>
2021-04-14 21:19:11 +0200

    The first commit.

---
+++ src/test/java/bm/app/watercollector/WaterCollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package bm.app.watercollector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaterCollectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
