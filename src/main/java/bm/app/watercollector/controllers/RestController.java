d56197dc4d992bf0f204d9d13be7304a715fe374
Dazikiri Anikar <mierzwinski.b@gmail.com>
2021-05-14 21:54:25 +0200

    Added @ApiOperation info for the REST endpoint.

--- src/main/java/bm/app/watercollector/controllers/RestController.java
+++ src/main/java/bm/app/watercollector/controllers/RestController.java
@@ -2,6 +2,7 @@ package bm.app.watercollector.controllers;
 
 import bm.app.watercollector.exception.ArrayTooSmallException;
 import bm.app.watercollector.service.WaterService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,6 +18,7 @@ public class RestController {
     }
 
     @GetMapping("/water/fillcount")
+    @ApiOperation(value = "Store the rain water.", notes = "API containing the algorithm for counting how much rain water can be stored in a provided array.")
     public int getNumberOfFieldsWaterFills(@RequestParam int[] val) throws ArrayTooSmallException {
         return waterService.maxWaterForRestApi(val);
     }
